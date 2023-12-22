dbb80273d43a002d17d8f8f16d0ab024cffaa719
Abenezer Seifu <abenezerseifu123@gmail.com>
2023-12-22 22:16:09 +0300

    Finalizing tailwindcss

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["src/Home.html", "src/Resume.html", "src/Projects.html", "src/Adventure.html","src/Entertainments.html", "src/Contact.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
